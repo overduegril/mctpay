87d2913f45bbf9dee53a7f07bc31a8e0f60a552f
guodongwei <1486733900@qq.com>
2020-02-24 16:15:36 +0800

    --!

--- manager-portal/src/main/java/com/mctpay/manager/controller/point/ManagerGiftController.java
+++ manager-portal/src/main/java/com/mctpay/manager/controller/point/ManagerGiftController.java
@@ -1,15 +1,9 @@
 package com.mctpay.manager.controller.point;
 
-import com.mctpay.common.base.model.ResponseData;
-import com.mctpay.manager.model.dto.point.ManagerGiftDTO;
-import com.mctpay.manager.model.dto.point.ManagerMemberLevelRulesDTO;
-import com.mctpay.manager.service.point.ManagerMemberLevelRulesService;
 import com.mctpay.manager.service.point.impl.ManagerGiftService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
