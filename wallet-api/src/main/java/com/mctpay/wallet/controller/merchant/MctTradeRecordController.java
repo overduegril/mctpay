e9dfb190d8d72a9caf656354c741646f68302e41
chenshubiao <2630298050@qq.com>
2020-06-17 18:28:41 +0800

    操作记录控制器提交

---
+++ wallet-api/src/main/java/com/mctpay/wallet/controller/merchant/MctTradeRecordController.java
@@ -0,0 +1,19 @@
+package com.mctpay.wallet.controller.merchant;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: chenshubiao
+ * @Description: 操作记录控制层
+ * @Date: 2020/6/17 15:01
+ */
+@Api(value = "操作记录相关", tags = "操作记录")
+@RestController
+@RequestMapping("wallet-mctTradeRecord")
+public class MctTradeRecordController {
+
+
+
+}
